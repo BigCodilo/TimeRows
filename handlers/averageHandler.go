d6f1380214b78f70349f7d51266882afcbd1f6bf
BigCodilo <vladgirko1@gmail.com>
2019-03-06 22:34:06 +0200

    now it's send html

--- handlers/averageHandler.go
+++ handlers/averageHandler.go
@@ -1,11 +1,12 @@
 package handlers
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 )
 
 //AverageHandlerGet - гет запрос с страничке /average, возвращает html клиетну
 func AverageHandlerGet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "qweqweqw")
+	tmpl, _ := template.ParseFiles("../static/average.html")
+	tmpl.ExecuteTemplate(w, "average", nil)
 }
--- index.go
+++ index.go
@@ -3,13 +3,15 @@ package main
 import (
 	"net/http"
 
+	"github.com/BigCodilo/TimeRows/handlers"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	r := mux.NewRouter()
+	r.PathPrefix("/static/").Handler(http.FileServer(http.Dir("./static/")))
 
-	//r.HandleFunc("/average", handlres.AverageHandlerGet).Methods("GET")
+	r.HandleFunc("/average", handlers.AverageHandlerGet).Methods("GET")
 
 	http.Handle("/", r)
 	http.ListenAndServe(":80", nil)
