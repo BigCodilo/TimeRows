bfcc0480ed287737e4afc37e18e7dc94cf0082fe
BigCodilo <vladgirko1@gmail.com>
2019-03-06 23:29:50 +0200

    test Ajax1

--- static/sendTimeRowAjax.js
+++ static/sendTimeRowAjax.js
@@ -1,6 +1,6 @@
 function SendTimeRow(){
     var ValueFromRowField = $("#time-row-field").val();
-    //var array = [];
+    var array = [];
 
     for (var i = 0; i < ValueFromRowField.length; i++) {
         console.console.log(ValueFromRowField.charAt(i));
